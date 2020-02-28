25489ad0fca48336156a6940f105c5b5713a7cca
PrathnaPel <prathnapel@gmail.com>
2020-02-28 16:05:23 -0500

    Update regex and parser

--- movie_scripts_scrapper.py
+++ movie_scripts_scrapper.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup
 import pandas as pd
 import re
 import time
+import numpy
 
 #Return HTML Source Page of Url
 def loadUrl(url,driver):
@@ -14,25 +15,26 @@ def loadUrl(url,driver):
 def getMovieScript(name,source):
     contents = BeautifulSoup(source, 'html.parser')
     preText = contents.pre.get_text()
-    text_file = open('./source_outputs/movies/'+name+'.txt', 'w')
+    text_file = open('./source_outputs/movies/'+name+'.txt','w')
     text_file.write(preText)
     text_file.close()
 
 #Write Movie Script to CSV
 def scriptParser(name):
-    file = open('./source_outputs/movies/'+name+'.txt',"r+") 
+    file = open('./source_outputs/movies/'+name+'.txt','r') 
     preText = file.readlines()
+    file.close()
     scriptText = []
     sceneText = []
     sceneText.append('')
     regstr = ''
     #Regex to Parse Movie Script 
-    pattern = re.compile(r'^\d.+\d\n?$|^\d+.$')
-    patternB = re.compile(r'^EXT\..+$|^INT\..+$')
-    patternC = re.compile(r'^I/E.+$')
+    pattern = re.compile(r'^\d.+\d\n?$|^\d+\s?.$',flags=re.MULTILINE)
+    patternB = re.compile(r'^EXT.+$|^INT.+$',flags=re.MULTILINE)
+    patternC = re.compile(r'^I/E.+$',flags=re.MULTILINE)
     for i in range(len(preText)):
         text = preText[i]
-        if pattern.search(text.strip()) != None or patternB.search(text.strip()) != None or patternC.search(text.strip()) != None:
+        if pattern.match(text.strip()) != None or patternB.match(text.strip()) != None or patternC.match(text.strip()) != None:
             sceneText.append(text.strip())
             if regstr:
                 scriptText.append(regstr)
@@ -46,7 +48,7 @@ def scriptParser(name):
     
     #DataFrame
     df_movie = pd.DataFrame({'Scenes':sceneText,'Scripts':scriptText})
-    df_movie.to_csv('./data/movies/'+name + '.csv',index=False)
+    df_movie.to_csv('./data/movies/'+name + '.csv',index=True)
 
 # Get Array of movie names from https://www.imsdb.com
 def getMovieTitles(source):
@@ -66,7 +68,7 @@ def movieTitlesToCsv(arr):
         title_url.append((tx,url))
     df = pd.DataFrame(title_url)
     #Trim unecessary
-    df = df.iloc[65:1275]
+    df = df.iloc[67:df.shape[0]-7]
     df.to_csv('./url/movies_url.csv',index=True)
 
 #Return movie url from title
@@ -88,7 +90,7 @@ if __name__ == "__main__":
         getMovieScript(ex1_tl,loadUrl(ex1_url,driver))
         getMovieScript(ex2_tl,loadUrl(ex2_url,driver))
         scriptParser(ex1_tl)
-        scriptParser(ex1_tl)
+        scriptParser(ex2_tl)
 
         #List of movies available on imsdb
         allMoviesUrl = "https://www.imsdb.com/all%20scripts/"
